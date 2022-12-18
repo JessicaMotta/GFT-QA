62b7d94087e35e201b862f7f758dd042d74c2a07
Jéssica Mota <jessicamotajobs@gmail.com>
2022-12-18 16:02:48 -0300

    adicionando estudos

---
+++ 2. Principios da programação e Testes com Java/trilha-java-basico/java-anatomia-classes/src/edu/jessica/anatomiaclasses/BoletimEstudantil.java
@@ -0,0 +1,15 @@
+//exemplo de identação: código fica mais legivel  
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if (mediaFinal<6){
+            System.out.println("Reprovado!");
+        } 
+        else if (mediaFinal==6){
+            System.out.println("Prova Minerva");
+        }
+        else{
+            System.out.println("Aprovado");
+        }
+    }
+}
